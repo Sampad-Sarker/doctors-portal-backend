768644de0e0b80cd90f91be5d968909dd2e5e5be
sampad-sarker <sampadsarker777@gmail.com>
2020-04-14 14:34:29 +0600

    nodemon added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-//const port = process.env.PORT || 5000 ||3001 ||4200;
+const port = process.env.PORT || 5000 ||3001 ||4200;
 
 
 
@@ -23,4 +23,4 @@ app.get('/', (req, res) => {
     res.send('Doctors portal backend!');
 })
 
-//app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
